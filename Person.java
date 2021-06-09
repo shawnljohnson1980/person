4c0bd707269ead535141980357840095604d72ca
shawnljohnson1980 <shawnljohnson1980@gmail.com>
2021-06-09 10:48:04 -0400

    person assignement

---
+++ Person.java
@@ -0,0 +1,5 @@
+public class Person {
+    public static void main(String name, String birthplace, Integer age) {
+        System.out.println("My Name is %s, my birthplace is %s, and I am %d years old.");
+    }
+}
